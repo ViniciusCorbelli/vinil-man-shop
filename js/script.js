a8d481269f83748e26dd866674470f54da546f2a
cabanezg <cabanezg@gmail.com>
2020-10-31 15:35:32 -0300

    Ajustando modais

--- js/script.js
+++ js/script.js
@@ -4,6 +4,8 @@ function toggleButton() {
     var btn = document.getElementById('sidenav');
     var navbar = document.getElementById("navbar");
     var main = document.getElementById('main');
+    var singOut = document.getElementById('sign-out');
+    var secondLine = document.getElementById('second-line');
     //console.log('click');
 
     if(toggled == false)
@@ -13,6 +15,10 @@ function toggleButton() {
         btn.style.width = "250px";
         navbar.style.marginLeft = "250px";
         main.style.marginLeft = "250px";
+        if (!window.matchMedia("(min-width: 760px)").matches) {
+            singOut.style.display = 'none';
+            secondLine.style.display = 'none';
+        }
     }
     else if(toggled == true) {
         toggled = false;
@@ -20,5 +26,8 @@ function toggleButton() {
         btn.style.width = "0";
         navbar.style.marginLeft = "0";
         main.style.marginLeft = "0";
+        singOut.style.display = 'block';
+        secondLine.style.display = 'inline-block';
+   
     }
 }
