7e03984bb1475ca087ba31e24dc81444c8cdc148
renanNun <renannunes400@gmail.com>
2020-11-01 14:29:11 -0300

    Atualizando scrit.js para responsividade da sidebar

--- js/script.js
+++ js/script.js
@@ -1,33 +1,60 @@
-var toggled = false
+toggled = true; //Booleano para controle da sidebar
+
+var btn = document.getElementById('sidenav'); //Botão Toggle que se localiza na navbar
+var navbar = document.getElementById("navbar"); //Navbar 
+var main = document.getElementById('main'); //O corpo da página
+var singOut = document.getElementById('sign-out'); //Elemento para realziar logout
+var secondLine = document.getElementById('second-line'); //Elemento para esconder a barra de pesquisa e de número de elementos em exibição
 
 function toggleButton() {
-    var btn = document.getElementById('sidenav');
-    var navbar = document.getElementById("navbar");
-    var main = document.getElementById('main');
-    var singOut = document.getElementById('sign-out');
-    var secondLine = document.getElementById('second-line');
-    //console.log('click');
+    
+    /* === Primeiro verificamos o tamanho da tela ===*/
+    if(window.matchMedia("(min-width: 1000px)").matches){
+        //A sidebar estará aberta
 
-    if(toggled == false)
-    {
-        toggled = true;
-        //console.log("ENTROU")
-        btn.style.width = "250px";
-        navbar.style.marginLeft = "250px";
-        main.style.marginLeft = "250px";
-        if (!window.matchMedia("(min-width: 760px)").matches) {
-            singOut.style.display = 'none';
-            secondLine.style.display = 'none';
+        if(toggled == true){
+            toggled = false;
+            btn.style.width = "0";
+            navbar.style.marginLeft = "0";
+            main.style.marginLeft = "0";
+        } else if(toggled == false){
+            toggled = true;
+            btn.style.width = "250px";
+            navbar.style.marginLeft = "250px";
+            main.style.marginLeft = "250px";
+        }
+
+    } else if(window.matchMedia("(min-width: 415px)").matches) { //Ipad, Ipad X
+        // A sidebar inicialmente está fechada
+
+        if(toggled == false){
+            toggled = true;
+            btn.style.width = "250px";
+            navbar.style.marginLeft = "250px";
+            main.style.marginLeft = "250px";
+            /*Se está fechado e tem largura menor que 760px, esconderemos o campo de pesquisa e o botão de logout */
+            if (!window.matchMedia("(min-width: 768px)").matches) {
+                singOut.style.display = 'none';
+                secondLine.style.display = 'none';
+            }
+        } else if(toggled == true){
+            toggled = false;
+            btn.style.width = "0";
+            navbar.style.marginLeft = "0";
+            main.style.marginLeft = "0";
+            singOut.style.display = 'block';
+            secondLine.style.display = 'flex';
+        }
+    } else if(window.matchMedia("(max-width: 414px)").matches){
+        /*A partir daqui a sidebar deverá cobrir toda a tela ao ser aberta */
+        if(toggled == true){
+            toggled = false;
+            btn.style.width = "100%";
         }
     }
-    else if(toggled == true) {
-        toggled = false;
-        //console.log("SAIU")
-        btn.style.width = "0";
-        navbar.style.marginLeft = "0";
-        main.style.marginLeft = "0";
-        singOut.style.display = 'block';
-        secondLine.style.display = 'flex';
-   
-    }
-}
+};
+
+function closeBtn(){
+    toggled = true;
+    btn.style.width = "0";
+};
