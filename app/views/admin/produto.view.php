534d2d8efd1770f4c650ffbe8e19a077c7f51593
Vinicius Corbelli <v.corbelli71@gmail.com>
2020-11-13 11:31:33 -0300

    Modais recebem a ID do produto

--- app/views/admin/produto.view.php
+++ app/views/admin/produto.view.php
@@ -54,58 +54,127 @@ require('C:\xampp\htdocs\app\views\partials/head.admin.php'); ?>
                         <tbody>
 
                             <?php foreach ($produtos as $produto) : ?>
-                            
+
                                 <tr>
-                                <td><?= $produto->name; ?></td>
-                                <td><?= $produto->price; ?></td>
-                                <td><?= $produto->id_category; ?></td>
-                                <td>
-                                    <button type="button" class="btn" data-toggle="modal" data-target="#view-product"><i class="fas fa-eye"></i></button>
-                                    <button type="button" class="btn" data-toggle="modal" data-target="#edit"><i class="fas fa-edit"></i></button>
-                                    <button type="button" class="btn" data-toggle="modal" data-target="#delete"><i class="fas fa-trash"></i></button>
-                                </td>
-                            </tr>
-                            
+                                    <td><?= $produto->name; ?></td>
+                                    <td><?= $produto->price; ?></td>
+                                    <td><?= $produto->id_category; ?></td>
+                                    <td>
+                                        <button type="button" class="btn" data-toggle="modal" data-target="#view-product-<?= $produto->id; ?>"><i class="fas fa-eye"></i></button>
+                                        <button type="button" class="btn" data-toggle="modal" data-target="#edit-<?= $produto->id; ?>"><i class="fas fa-edit"></i></button>
+                                        <button type="button" class="btn" data-toggle="modal" data-target="#delete-<?= $produto->id; ?>"><i class="fas fa-trash"></i></button>
+                                    </td>
+                                </tr>
+
+
+                                <!--MODAIS DO BOOTSTRAP-->
+                                <!--deletar-->
+                                <div class="modal" id="delete-<?= $produto->id; ?>" tabindex="-1" role="dialog">
+                                    <div class="modal-dialog modal-dialog-centered" role="document">
+                                        <div class="modal-content">
+                                            <div class="modal-header">
+                                                <h5 class="modal-title">Confirmação para deletar</h5>
+                                                <button type="button" class="close" data-dismiss="modal" aria-label="Close">
+                                                    <span aria-hidden="true">&times;</span>
+                                                </button>
+                                            </div>
+                                            <div class="modal-body">
+                                                <p>Você confirma que deseja deletar o produto: <?= $produto->name; ?></p>
+                                                <p>Esta ação é irrevessível</p>
+                                            </div>
+                                            <div class="modal-footer">
+                                                <button type="button" class="btn btn-secondary" data-dismiss="modal">Fechar</button>
+                                                <button type="button" class="btn btn-danger">Confirmar</button>
+                                            </div>
+                                        </div>
+                                    </div>
+                                </div>
+
+                                <!--view-product-->
+                                <div class="modal" id="view-product-<?= $produto->id; ?>" tabindex="-1" role="dialog">
+                                    <div class="modal-dialog modal-dialog-centered" role="document">
+                                        <div class="modal-content">
+                                            <div class="modal-header">
+                                                <h5 class="modal-title">Visualizar</h5>
+                                                <button type="button" class="close" data-dismiss="modal" aria-label="Close">
+                                                    <span aria-hidden="true">&times;</span>
+                                                </button>
+                                            </div>
+                                            <div class="modal-body">
+                                                <div class="view-product-container">
+                                                    <img src="$produto->image;" alt="Imagem do produto">
+                                                    <div class="view-product-information">
+                                                        <?= $produto->description; ?>
+                                                    </div>
+                                                </div>
+                                            </div>
+                                            <div class="modal-footer">
+                                                <button type="button" class="btn btn-secondary" data-dismiss="modal">Fechar</button>
+                                            </div>
+                                        </div>
+                                    </div>
+                                </div>
+
+
+                                <!--Edit-->
+                                <div class="modal" id="edit-<?= $produto->id; ?>" tabindex="-1" role="dialog">
+                                    <div class="modal-dialog modal-dialog-centered" role="document">
+                                        <div class="modal-content">
+                                            <div class="modal-header">
+                                                <h5 class="modal-title">Editar Produto</h5>
+                                                <button type="button" class="close" data-dismiss="modal" aria-label="Close">
+                                                    <span aria-hidden="true">&times;</span>
+                                                </button>
+                                            </div>
+                                            <div class="modal-body">
+                                                <div class="form-group">
+                                                    <label>Nome do produto</label>
+                                                    <input type="text" class="form-control" placeholder="Nome do produto">
+                                                </div>
+                                                <div class="form-group">
+                                                    <label>Preço(R$)</label>
+                                                    <input type="text" class="form-control" placeholder="Preço do produto">
+                                                </div>
+
+                                                <div class="form-group">
+                                                    <label>Descrição</label>
+                                                    <input type="text" class="form-control" placeholder="Descrição do produto">
+                                                </div>
+                                                <div class="form-group">
+                                                    <label>Categoria</label>
+                                                    <select class="select-category form-control">
+                                                        <option selected>Categoria</option>
+                                                </div>
+                                                <div class="form-group">
+                                                    <label>Foto</label>
+                                                    <input type="image" src="" class="form-control" alt="Adicione a imagem do produto" style="height: 200px;">
+                                                </div>
+                                            </div>
+                                            <div class="modal-footer">
+                                                <button type="button" class="btn btn-secondary" data-dismiss="modal">Fechar</button>
+                                                <button type="button" class="btn btn-success">Editar</button>
+                                            </div>
+                                        </div>
+                                    </div>
+                                </div>
+
+
                             <?php endforeach; ?>
                         </tbody>
                     </table>
                     <nav class="nav justify-content-end">
                         <ul class="pagination">
-                            <li class="page-item"><a class="page-link" href="#">Previous</a></li>
+                            <li class="page-item"><a class="page-link" href="#">Anterior</a></li>
                             <li class="page-item"><a class="page-link" href="#">1</a></li>
                             <li class="page-item"><a class="page-link" href="#">2</a></li>
                             <li class="page-item"><a class="page-link" href="#">3</a></li>
-                            <li class="page-item"><a class="page-link" href="#">Next</a></li>
+                            <li class="page-item"><a class="page-link" href="#">Proximo</a></li>
                         </ul>
                     </nav>
                 </div>
             </div>
         </div>
     </main>
-</div>
-</div>
-
-<!--MODAIS DO BOOTSTRAP-->
-<!--deletar-->
-<div class="modal" id="delete" tabindex="-1" role="dialog">
-    <div class="modal-dialog modal-dialog-centered" role="document">
-        <div class="modal-content">
-            <div class="modal-header">
-                <h5 class="modal-title">Confirmação para deletar</h5>
-                <button type="button" class="close" data-dismiss="modal" aria-label="Close">
-                    <span aria-hidden="true">&times;</span>
-                </button>
-            </div>
-            <div class="modal-body">
-                <p>Você confirma que deseja deletar o produto {{Nome do produto}}</p>
-                <p>Esta ação é irrevessível</p>
-            </div>
-            <div class="modal-footer">
-                <button type="button" class="btn btn-secondary" data-dismiss="modal">Fechar</button>
-                <button type="button" class="btn btn-danger">Confirmar</button>
-            </div>
-        </div>
-    </div>
 </div>
 
 <!--criar novo-->
@@ -144,75 +213,6 @@ require('C:\xampp\htdocs\app\views\partials/head.admin.php'); ?>
             <div class="modal-footer">
                 <button type="button" class="btn btn-secondary" data-dismiss="modal">Fechar</button>
                 <button type="button" class="btn btn-success">Criar novo</button>
-            </div>
-        </div>
-    </div>
-</div>
-
-<!--view-product-->
-<div class="modal" id="view-product" tabindex="-1" role="dialog">
-    <div class="modal-dialog modal-dialog-centered" role="document">
-        <div class="modal-content">
-            <div class="modal-header">
-                <h5 class="modal-title">Visualizar</h5>
-                <button type="button" class="close" data-dismiss="modal" aria-label="Close">
-                    <span aria-hidden="true">&times;</span>
-                </button>
-            </div>
-            <div class="modal-body">
-                <div class="view-product-container">
-                    <img src="/img/disco-teste.jpg" alt="Produto-1">
-                    <div class="view-product-information">
-                        <h4>Descrição</h4>
-                        <p>Disco da banda Guns 'N Roses</p>
-                    </div>
-                </div>
-            </div>
-            <div class="modal-footer">
-                <button type="button" class="btn btn-secondary" data-dismiss="modal">Fechar</button>
-            </div>
-        </div>
-    </div>
-</div>
-
-
-<!--Edit-->
-<div class="modal" id="edit" tabindex="-1" role="dialog">
-    <div class="modal-dialog modal-dialog-centered" role="document">
-        <div class="modal-content">
-            <div class="modal-header">
-                <h5 class="modal-title">Editar Produto</h5>
-                <button type="button" class="close" data-dismiss="modal" aria-label="Close">
-                    <span aria-hidden="true">&times;</span>
-                </button>
-            </div>
-            <div class="modal-body">
-                <div class="form-group">
-                    <label>Nome do produto</label>
-                    <input type="text" class="form-control" placeholder="Nome do produto">
-                </div>
-                <div class="form-group">
-                    <label>Preço(R$)</label>
-                    <input type="text" class="form-control" placeholder="Preço do produto">
-                </div>
-
-                <div class="form-group">
-                    <label>Descrição</label>
-                    <input type="text" class="form-control" placeholder="Descrição do produto">
-                </div>
-                <div class="form-group">
-                    <label>Categoria</label>
-                    <select class="select-category form-control">
-                        <option selected>Categoria</option>
-                </div>
-                <div class="form-group">
-                    <label>Foto</label>
-                    <input type="image" src="" class="form-control" alt="Adicione a imagem do produto" style="height: 200px;">
-                </div>
-            </div>
-            <div class="modal-footer">
-                <button type="button" class="btn btn-secondary" data-dismiss="modal">Fechar</button>
-                <button type="button" class="btn btn-success">Editar</button>
             </div>
         </div>
     </div>
