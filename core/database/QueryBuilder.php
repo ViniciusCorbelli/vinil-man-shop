3064c27f58675a15c1f4a18d323e097337889285
LeonardoNF686 <leonardonvfe@gmail.com>
2020-11-15 17:00:20 -0300

    query edit

--- core/database/QueryBuilder.php
+++ core/database/QueryBuilder.php
@@ -41,11 +41,16 @@ class QueryBuilder
 
     public function editProduct($table, $parameters, $id)
     {
-        $sql = sprintf(
-            "update product set name = '{$parameters['name']}', description = '{$parameters['description']}',
-            image = '{$parameters['image']}', price = '{$parameters['price']}', stock = '{$parameters['stock']}',
-            id_category = '{$parameters['id_category']}' where id = {$id}"
-        );
+        $last = end($parameters);
+
+        $sql = "update {$table} set ";
+         foreach($parameters as $item => $val) {
+             if($val == "$last"){
+                $sql = $sql . "$item = '{$val}' ";
+             }else{
+             $sql = $sql . "$item = '{$val}', ";}
+         };
+        $sql = $sql . "where id = {$id}";
 
         try {
             $statement = $this->pdo->prepare($sql);
