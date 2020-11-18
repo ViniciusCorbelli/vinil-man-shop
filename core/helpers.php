4ae8bc142c83a3a9a0bf06e277c76f2cc3a5f45f
PedroAlexandre2020 <pedro.alexandre_fernandes@hotmail.com>
2020-11-18 20:10:47 -0300

    msg

--- core/helpers.php
+++ core/helpers.php
@@ -1,13 +1,25 @@
 <?php
 
-function view($name, $data = [])
+/**
+ * Require a view.
+ *
+ * @param  string $name
+ * @param  array  $data
+ */
+
+
+function viewadmin($name, $data = [])
 {
     extract($data);
 
     return require "app/views/admin/{$name}.view.php";
 }
 
-
+/**
+ * Redirect to a new page.
+ *
+ * @param  string $path
+ */
 function redirect($path)
 {
     header("Location: /{$path}");
