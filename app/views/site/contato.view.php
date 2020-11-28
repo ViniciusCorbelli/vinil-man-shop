05d02df4b699212e2bef3937ed3c8e8f9db24d56
renanNun <renannunes400@gmail.com>
2020-11-28 12:27:37 -0300

    The Mobile tinymce adicionado

--- app/views/site/contato.view.php
+++ app/views/site/contato.view.php
@@ -29,18 +29,22 @@
             </div>
 
 
-    <script src="https://cdn.tiny.cloud/1/amcj75jc93ophbe34wfi6y9q7jnrom6tv0wtsh2du10fuo0l/tinymce/5/tinymce.min.js" referrerpolicy="origin"></script>
-    <script>
-        tinymce.init({
-            selector: "#editor",
-            language: "pt_BR",
-            plugins: [
-                'advlist autolink link image lists preview hr anchor pagebreak',
-                'searchreplace wordcount code fullscreen insertdatetime media nonbreaking',
-                'emoticons'
-            ],
-            menubar: 'favs file edit view insert format tools table help',
-            toolbar: 'insertfile undo redo | styleselect | bold italic | alignleft aligncenter alignright alignjustify | bullist numlist outdent indent | link image | print preview media fullpage | forecolor backcolor emoticons'
-        });
-    </script>
-    <?php require('app/views/partials/footer.php') ?>
+            <script src="https://cdn.tiny.cloud/1/amcj75jc93ophbe34wfi6y9q7jnrom6tv0wtsh2du10fuo0l/tinymce/5/tinymce.min.js" referrerpolicy="origin"></script>
+            <script>
+                tinymce.init({
+                    selector: "#editor",
+                    language: "pt_BR",
+                    plugins: [
+                        'advlist autolink link image preview hr anchor pagebreak',
+                        'searchreplace wordcount insertdatetime media nonbreaking',
+                        'emoticons'
+                    ],
+                    menubar: 'favs file edit view insert format tools table help',
+                    toolbar: 'insertfile undo redo | styleselect | bold italic | alignleft aligncenter alignright alignjustify | bullist numlist outdent indent | link image | print preview media fullpage | forecolor backcolor emoticons',
+                    height: "300",
+                    mobile: {
+                        theme: 'mobile'
+                    }
+                });
+            </script>
+            <?php require('app/views/partials/footer.php') ?>
