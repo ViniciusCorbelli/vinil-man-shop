ef422aefc845542939545fb479a9f1352c771601
renanNun <renannunes400@gmail.com>
2020-11-28 11:43:36 -0300

    Revendo escrita da página de contato, diminuindo quantidade de extensões do tinymce

--- app/views/site/contato.view.php
+++ app/views/site/contato.view.php
@@ -28,25 +28,6 @@
 
             </div>
 
-            <div class="contato1 ">
-
-                <h1> Ligue para nós </h1>
-                <i class="fas fa-phone fa-3x my-4"></i>
-                <h2>(021)98765-4321</h2>
-                <h1> Ou nos mande uma mensagem </h1>
-                <i class="fab fa-instagram fa-4x my-4"></i>
-                <h2>@vinilman<br>
-                    <i class="far fa-envelope fa-2x my-4"></i><br>vinilman@gmail.com</h2>
-            </div>
-
-            <div class="mapa my-4">
-                <h1> Visite A gente! </h1>
-                <div class="mapa">
-                    <iframe id="mapa_class" src="https://www.google.com/maps/embed?pb=!1m14!1m8!1m3!1d14820.449024741118!2d-43.372468!3d-21.775916!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x989ba191fb377d%3A0xb200458b94cba93a!2sVia%20Local%2C%204569%20-%20S%C3%A3o%20Pedro%2C%20Juiz%20de%20Fora%20-%20MG%2C%2036036-900!5e0!3m2!1spt-BR!2sbr!4v1603489818966!5m2!1spt-BR!2sbr" frameborder="7" style="border:0;" allowfullscreen="" aria-hidden="false" tabindex=""></iframe>
-                </div>
-            </div>
-        </div>
-    </div>
 
     <script src="https://cdn.tiny.cloud/1/amcj75jc93ophbe34wfi6y9q7jnrom6tv0wtsh2du10fuo0l/tinymce/5/tinymce.min.js" referrerpolicy="origin"></script>
     <script>
@@ -54,16 +35,10 @@
             selector: "#editor",
             language: "pt_BR",
             plugins: [
-                'advlist autolink link image lists charmap print preview hr anchor pagebreak',
-                'searchreplace wordcount visualblocks visualchars code fullscreen insertdatetime media nonbreaking',
-                'table emoticons template paste help'
+                'advlist autolink link image lists preview hr anchor pagebreak',
+                'searchreplace wordcount code fullscreen insertdatetime media nonbreaking',
+                'emoticons'
             ],
-            menu: {
-                favs: {
-                    title: 'Meus favoritos',
-                    items: 'code visualaid | searchreplace | emoticons'
-                }
-            },
             menubar: 'favs file edit view insert format tools table help',
             toolbar: 'insertfile undo redo | styleselect | bold italic | alignleft aligncenter alignright alignjustify | bullist numlist outdent indent | link image | print preview media fullpage | forecolor backcolor emoticons'
         });
