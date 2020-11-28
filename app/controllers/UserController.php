88f177d8ef61205cc76b8188d4aa294823307765
Vinicius Corbelli <v.corbelli71@gmail.com>
2020-11-27 21:14:37 -0300

    Correção no controller de usuario

--- app/controllers/UserController.php
+++ app/controllers/UserController.php
@@ -1,66 +1,128 @@
 <?php
 
 namespace App\Controllers;
+
 use app\core\App;
 use Exception;
 
-class UserController{
+class UserController
+{
 
     public function index()
     {
-        $users = App::get('database')->selectAll('users');
+        $users = App::get('database')->selectAllUsers();
 
-        return view('/admin/usuario', compact('users'));
+        return view('admin/usuario',[
+            'users' => $users
+        ]);
     }
 
     public function create()
     {
-        $user = App::get('database')->search('users',['email' => $_POST['email']]);
-        
-        if(count($user) > 0)
-        {
+        session_start();
+        $_SESSION['sucessos'] = ([]);
+        $_SESSION['erros'] = ([]);
+
+
+        //Tratando a criação de senhas
+        $password = $_POST['password']; //Senha
+        $password_controller = $_POST['password-controller']; //Confirme sua senha
+        $email = $_POST['email'];
+        $nome = $_POST['name'];
+
+        if(empty($password) || empty($password_controller) || empty($email) || empty($nome))
+        {   
+            $_SESSION['erros'][] = "Os Campos não podem ficar em branco e devem conter um formato válido";
             return redirect('admin/usuarios');
+        }
+        else if($password == $password_controller)
+        {
+            $user = App::get('database')->search('users', ['email' => $_POST['email']]);
+
+            if (count($user) > 0) {
+                $_SESSION['erros'][] = "O Email inserido já existe no sistema!";
+                return redirect('admin/usuarios');
+            } else {
+    
+                $hash = password_hash($_POST['password'], PASSWORD_DEFAULT);
+    
+                $parameters = ([
+                    'name' => $_POST['name'],
+                    'email' => $_POST['email'],
+                    'password' => $hash
+                ]);
+    
+                App::get('database')->insert('users', $parameters);
+
+                $_SESSION['sucessos'][] = "Usuário criado com sucesso!";   
+                return redirect('admin/usuarios');
+            }
         } else {
+            $_SESSION['erros'][] = "Algo inesperado aconteceu!";
 
-        $hash = password_hash($_POST['password'], PASSWORD_DEFAULT);
-
-        $parameters = ([
-            'name' => $_POST['name'],
-            'email' => $_POST['email'],
-            'password' => $hash
-        ]);
-
-        App::get('database')->insert('users',$parameters);
-
-        header('Location: /admin/usuarios');
+            return redirect('admin/usuarios');
         }
     }
 
     public function edit()
     {
-        $parameters = ([
-            'name' => $_POST['name'],
-            'email' => $_POST['email']
-        ]);
+        session_start();
+        $_SESSION['sucessos'] = ([]);
+        $_SESSION['erros'] = ([]);
 
-        App::get('database')->edit('users', $parameters, $_POST['id']);
-
-        header('Location: /admin/usuarios');
-    }
-
-    public function delete()
-    {
-        $id = $_POST['id'];
+        if(empty($_POST['name']))
+        {
+            $_SESSION['erros'][] = "Você deve inserir um nome!";
+        }
+        if(empty($_POST['email']))
+        {
+            $_SESSION['erros'][] = "Você deve inserir um email válido!";
+        }
 
         try{
-            App::get('database')->delete('users', $id);
+            $parameters = ([
+                'name' => $_POST['name'],
+                'email' => $_POST['email']
+            ]);
+    
+            App::get('database')->edit('users', $parameters, $_POST['id']);
+                
+            $_SESSION['sucessos'][] = "Usuário editado com sucesso!";    
 
-            header('Location: /admin/usuarios');
+            return redirect('admin/usuarios');
         }catch(Exception $e)
         {
             $e->getMessage();
+
+            $_SESSION['erros'][] = "Algo inesperado aconteceu!";
+
+            return redirect('admin/usuarios');
         }
 
     }
 
+    public function delete()
+    {
+        session_start();
+        $_SESSION['sucessos'] = ([]);
+        $_SESSION['erros'] = ([]);
+
+
+        try{
+            $id = $_POST['id'];
+
+            App::get('database')->delete('users', $id);
+            
+            $_SESSION['sucessos'][] = "Usuário deletado com sucesso!";
+
+            return redirect('admin/usuarios');
+        }catch(Exception $e)
+        {
+            $e->getMessage();
+
+            $_SESSION['erros'][] = "Algo inesperado aconteceu!";
+
+            return redirect('admin/usuarios');
+        }
+    }
 }
