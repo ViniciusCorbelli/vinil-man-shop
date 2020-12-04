5f75a56166a923a13929aad8cf8ed7e43da5a8c7
renanNun <renannunes400@gmail.com>
2020-12-04 18:38:42 -0300

    Correção no erro do login

--- app/controllers/PagesController.php
+++ app/controllers/PagesController.php
@@ -138,7 +138,6 @@ class PagesController extends LoginController
     public function login()
     {
         session_start();
-        $_SESSION['erros'] = ([]);
 
         //die(var_dump($_SESSION));
         if (isset($_SESSION) && isset($_SESSION['logged'])) {
